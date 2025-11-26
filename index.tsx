978e3d1bb51fbf1c24b2b6a9b57723ca0121dbee
pankaj kumar <pankajamsali@gmail.com>
2025-11-26 11:54:13 +0530

    Add Vercel Analytics integration and update dependencies in package.json and package-lock.json

--- index.tsx
+++ index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Analytics } from '@vercel/analytics/next';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <Analytics />
+  </>
 );
